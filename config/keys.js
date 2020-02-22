5ed70e7c536eeaeb7102f3a9d8b7ae41e15635db
Mason <masondguy@gmail.com>
2020-02-22 18:04:16 -0500

    Revert "Delete keys.js"
    This reverts commit 4a3fa071c482995c7b43b92f2ecd0e53a19c1cfa.

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: "mongodb+srv://masondev:masondev@testdb-harbp.mongodb.net/test?retryWrites=true&w=majority",
+    mongoURI_LOCAL: "mongodb://localhost/test",
+    SECERT_OR_KEY:"w?J[mg:@>+^GC^$(n1j@Xi35:0S>IT~9<xU|Kfn#U.RR$gQ;%kLWjfpDZE1r/"
+}
