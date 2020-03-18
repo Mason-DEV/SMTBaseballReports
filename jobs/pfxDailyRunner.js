95ed778601f651eb9d9cca3e35b99f5ecfc0439e
Mason <32374018+Mason-DEV@users.noreply.github.com>
2020-03-17 23:46:17 -0400

    Update pfxDailyRunner.js

--- jobs/pfxDailyRunner.js
+++ jobs/pfxDailyRunner.js
@@ -22,13 +22,13 @@ var mailOptions = {
 
 
 async function ExecutePfxDaily(){
-    transporter.sendMail(mailOptions, function(error, info){
-      if (error) {
-        console.log(error);
-      } else {
-        console.log('Email sent: ' + info.response);
-      }
-    });
+//     transporter.sendMail(mailOptions, function(error, info){
+//       if (error) {
+//         console.log(error);
+//       } else {
+//         console.log('Email sent: ' + info.response);
+//       }
+//     });
     
 }
 
